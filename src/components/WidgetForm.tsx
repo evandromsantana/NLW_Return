3001244b0bc1e9826046cdd36c185a53e34c2c06
EVANDRO DE MACEDO SANTANA <101930026+evandromsantana@users.noreply.github.com>
2022-05-04 23:29:11 -0300

    object comentado

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -4,7 +4,7 @@ import bugImageUrl from '../assets/bug.svg';
 import ideaImageUrl from '../assets/idea.svg';
 import thoughtImageUrl from '../assets/thought.svg';
 
-const feedbackTypes = {
+export const feedbackTypes = {
     BUG: {
         title: 'Problema',
         image: {
@@ -24,17 +24,19 @@ const feedbackTypes = {
     },
 };
 
+
 export function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>
-
-                <CloseButton />
+               <span className="text-xl leading-6">Deixe seu feedback</span>
+              
+              <CloseButton />
             </header>
 
             <div className="flex py-8 gap-2 w-full">
-                { Object.entries(feedbackTypes).map(([key, value]) => {                 
+                
+                {/* Object.entries(feedbackTypes).map(([key, value]) => {                 
                     return (
                         <button
                             key={key}
@@ -45,11 +47,12 @@ export function WidgetForm() {
                             <span>{value.title}</span>
                         </button>
                     )
-                }) }
+                }) */}
             </div>
+             
 
             <footer className="text-xs text-neutral-400">
-                Feito com ♥ por <a className="underline underline-offset-2" href="https://evandrosantana.com">Evandro</a>
+                Feito com ♥ por <a className="underline underline-offset-2" href="https://www.linkedin.com/in/evandro-m-santana/">Evandro</a>
             </footer>
         </div>
     );
