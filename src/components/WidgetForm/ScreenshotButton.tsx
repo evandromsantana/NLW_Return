00192257a7923213bad4f7e5171ea6955702e10f
EVANDRO DE MACEDO SANTANA <101930026+evandromsantana@users.noreply.github.com>
2022-05-05 23:57:40 -0300

    aplicacao finalizada

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,15 +1,45 @@
 import html2canvas from "html2canvas";
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import { useState } from "react";
+import { Loading } from "../Loading";
 
-export function ScreenshotButton() {
+interface ScreenshotButtonProps {
+    screenshot: string | null;
+    onScreenshotTook: (screenshot: String | null) => void;
+}
+
+export function ScreenshotButton({ 
+    screenshot, 
+    onScreenshotTook 
+}: ScreenshotButtonProps) {
     const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
 
     async function handleTakenScreenshot() {
+        setIsTakingScreenshot(true);
+
         const canvas = await html2canvas(document.querySelector('html')!);
         const base64image = canvas.toDataURL('image/png');
 
-        console.log(base64image);
+        onScreenshotTook(base64image);
+        setIsTakingScreenshot(false);
+    }
+
+    if (screenshot) {
+        return (
+            <button
+                type="button"
+                className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+                onClick={() => onScreenshotTook(null)}
+                style={{
+                    backgroundImage: `url(${screenshot})`,
+                    // obs: mudar posição de prt da pagina no site depois configurar as duas linhas abaixo
+                    backgroundPosition:'right bottom',
+                    backgroundSize: 180,
+                }}
+            >                    
+                <Trash weight="fill" />
+            </button>
+        );
     }
 
     return (
@@ -18,7 +48,7 @@ export function ScreenshotButton() {
             onClick={handleTakenScreenshot}
             className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
         >
-            <Camera className="w-6 h-6"  />
+            { isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6" /> }
         </button>
     )
 }
